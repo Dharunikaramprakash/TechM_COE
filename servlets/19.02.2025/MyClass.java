dcdefc9fb29437464c1157691791815b43aeec24
Dharunikaramprakash <146358046+Dharunikaramprakash@users.noreply.github.com>
2025-02-20 08:07:58 +0530

    Update MyClass.java

--- servlets/19.02.2025/MyClass.java
+++ servlets/19.02.2025/MyClass.java
@@ -1,1 +1,19 @@
+package pack1;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class MyClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+}
+}
